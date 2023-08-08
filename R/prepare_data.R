7810e0668a1fc8809e61a1ead1e16370d6ad5984
Maria del Mar Quiroga <mar.quiroga@unimelb.edu.au>
2023-08-08 15:07:59 +1000

    Move data prep to its own function

---
+++ R/prepare_data.R
@@ -0,0 +1,61 @@
+
+#' Prepares data for reciprobit plot
+#'
+#' @param rt Vector of reaction times for a single participant, or a dataframe
+#' with a column called `times` with the reaction times and another called
+#' `color` that contains one hexadecimal color code for each participant.
+#' Optionally may contain a column `name` to use as labels for each dataset.
+#' @param time_units Units of the reaction times in rt_vector,
+#' must be one of "ms", "ds", or "s".
+#'
+#' @return A dataframe with columns: `times`, `color`, `name`, `promptness`, and `e_cdf`.
+#'
+#' @export
+#'
+#' @examples
+#' df <- prepare_data(rt)
+prepare_data <- function(rt, time_units = "ms") {
+  if(typeof(rt) == "double"){
+    plot_data <- data.frame(times = convert_to_seconds(rt, time_units = time_units)) %>%
+      dplyr::mutate(color = factor("#1B9E77"))
+  } else if(typeof(rt) == "list") {
+    plot_data <- rt %>%
+      dplyr::mutate(times = convert_to_seconds(.data$times, time_units = time_units))
+  }
+
+  # If no names, create dataset names from unique colors
+  if(!"name" %in% colnames(plot_data)){
+    plot_data <- transform(plot_data, name = factor(match(plot_data$color, unique(plot_data$color))))
+  }
+
+  plot_data %>%
+    dplyr::arrange(.data$color, .data$times) %>%
+    dplyr::group_by(.data$color) %>%
+    dplyr::mutate(
+      promptness = 1/.data$times,
+      e_cdf = stats::ecdf(.data$times)(.data$times)
+    ) %>%
+    dplyr::filter(.data$e_cdf < 1)
+
+}
+
+#' Convert reaction time vector to seconds. Used only internally, so not exported
+#'
+#' @param rt_vector Vector of reaction times, with type 'double'
+#' @param time_units Units of rt_vector, must be one of "ms", "ds", or "s"
+#'
+#' @return rt_vector in seconds
+
+convert_to_seconds <- function(rt_vector, time_units = "ms") {
+
+  if(time_units == "ms"){
+    rt_vector/1000
+  } else if(time_units == "ds") {
+    10*rt_vector
+  } else if(time_units == "s") {
+    rt_vector
+  } else {
+    rlang::abort('`time_units` must be one of "ms", "ds", or "s".')
+  }
+
+}
--- R/reciprobit_plot.R
+++ R/reciprobit_plot.R
@@ -1,6 +1,7 @@
 #' Plot reaction times in reciprobit axes
 #'
-#' @param rt vector of reaction times
+#' @param plot_data A dataframe with columns: `times`, `color`, `name`, `promptness`,
+#' and `e_cdf`; one color per name (participant)
 #'
 #' @return A reciprobit plot with the cumulative probability distribution of the reaction times
 #' @export
@@ -8,19 +9,17 @@
 #' @importFrom rlang .data
 #'
 #' @examples
-#' reciprobit_plot(rt)
+#' df <- prepare_data(rt2)
+#' reciprobit_plot(df)
 
-reciprobit_plot <- function(rt){
+reciprobit_plot <- function(plot_data){
 
   time_breaks   <- c(0.1,0.2,0.3,0.5,1)
   probit_breaks <- c(1,5,10,20,50,80,90,95,99)
   Z_breaks      <- c(-2, -1, 0, 1, 2)
 
-  plot_data <- data.frame(times = sort(rt/1000)) %>%
-    dplyr::mutate(promptness = 1/.data$times, e_cdf = sort(stats::ecdf(.data$times)(.data$times))) %>%
-    dplyr::filter(.data$e_cdf < 1)
 
-  ggplot2::ggplot(plot_data, ggplot2::aes(x=.data$promptness, y=.data$e_cdf)) +
+  ggplot2::ggplot(plot_data, ggplot2::aes(x=.data$promptness, y=.data$e_cdf, colour = .data$color)) +
     ggplot2::geom_point() +
     ggplot2::scale_x_reverse(
       # Main axis
@@ -34,6 +33,11 @@ reciprobit_plot <- function(rt){
       # Secondary axis
       sec.axis = ggplot2::sec_axis(trans = stats::qnorm, name="Z-score", breaks = Z_breaks)
     ) +
-    ggplot2::theme_minimal()
+    ggplot2::scale_color_manual(values = levels(plot_data$color), labels = levels(plot_data$name)) +
+    ggplot2::theme_minimal() +
+    ggplot2::labs(color = "") +
+    ggplot2::theme(
+      panel.grid.minor = ggplot2::element_line(linetype = 2)
+    )
 
 }
