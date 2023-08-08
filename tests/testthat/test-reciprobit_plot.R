d95c77b55c442b67fddd6a9ec65ee2fcfcbb33ee
Maria del Mar Quiroga <mar.quiroga@unimelb.edu.au>
2023-08-08 15:08:30 +1000

    Test reciprobit plots for both example datasets

--- tests/testthat/test-reciprobit_plot.R
+++ tests/testthat/test-reciprobit_plot.R
@@ -1,5 +1,13 @@
-test_that("Plotting in reciprobit space works as expected", {
+test_that("Plot one dataset in reciprobit space", {
   #TODO: look into https://ggplot2.tidyverse.org/articles/ggplot2-in-packages.html#testing-ggplot2-output
-  disp_reciprobit <- reciprobit_plot(rt)
-  vdiffr::expect_doppelganger("Plot in reciprobit space", disp_reciprobit)
+  df <- prepare_data(rt)
+  disp_reciprobit <- reciprobit_plot(df)
+  vdiffr::expect_doppelganger("One dataset in reciprobit space", disp_reciprobit)
+})
+
+test_that("Plot two datasets in reciprobit space", {
+  #TODO: look into https://ggplot2.tidyverse.org/articles/ggplot2-in-packages.html#testing-ggplot2-output
+  df <- prepare_data(rt2)
+  disp_reciprobit <- reciprobit_plot(df)
+  vdiffr::expect_doppelganger("Two datasets in reciprobit space", disp_reciprobit)
 })
