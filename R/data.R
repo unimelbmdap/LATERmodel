82dbb1294b0246f5095444c02f56499e7ed9edde
Maria del Mar Quiroga <mar.quiroga@unimelb.edu.au>
2023-08-08 15:06:04 +1000

    Add dataset with two participants and document

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' LATER unit reaction time data
+#'
+#' Simulated data from SPIC
+#'
+#' @format ## `rt`
+#' A vector of 200 reaction times
+#' \describe{
+#'   \item{rt}{Vector of reaction times in ms}
+#' }
+#' @source LATER 1 mu 5 sig 0.5 R, SPIC program, developed by R.H.S. Carpenter
+"rt"
+
+#' Two LATER units reaction time data
+#'
+#' Simulated data from SPIC
+#'
+#' @format ## `rt2`
+#' A dataframe of 200 reaction times for two participants: mu3 and mu4
+#' \describe{
+#'   \item{times}{Reaction times in ms}
+#'   \item{name}{Participant name}
+#'   \item{color}{Color to use for plotting each participant}
+#' }
+#' @source mu3 = LATER 1 mu 3 sig 0.5 R, count = 200; mu4 = LATER 2 mu 4 sig 0.5 R, count = 200 - SPIC program, developed by R.H.S. Carpenter
+"rt2"
