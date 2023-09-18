25fc78e894952c9c75fc14dd8b65625eb17b84f5
Damien Mannion <damien.mannion@unimelb.edu.au>
2023-09-18 15:41:25 +1000

    -

--- R/fit_data.R
+++ R/fit_data.R
@@ -1,5 +1,5 @@
 #' Fit a LATER model to a single dataset or a pair of datasets.
-#' 
+#'
 #' @param data A data frame with columns `name` and `promptness`.
 #' @param share_a,share_sigma,share_sigma_e If `FALSE` (the default), each
 #'  dataset has its own parameter. If `TRUE`, the datasets share the relevant
@@ -12,12 +12,18 @@
 #' @param use_minmax If `FALSE` (the default), the optimiser targets the sum
 #'  of the goodness-of-fit values across datasets; if `TRUE`, it instead
 #'  targets the maximum of the goodness-of-fit values across datasets.
-#' @returns A numeric vector.
+#' @returns A list of fitting arguments and outcomes.
+#' * `fitted_params` is a named list of fitted parameter values.
+#' * `loglike` is the overall log-likelihood of the fit.
+#' * `optim_result` is the raw output from `stats::optim`.
 #' @examples
 #' data <- data.frame(name = "test", promptness = rnorm(100, 3, 1))
 #' fit <- fit_data(data = data)
 #' data_other <- data.frame(name = "test_2", promptness = rnorm(100, 1, 1))
-#' fit_shared_sigma <- fit_data(data = rbind(data, data_other), share_sigma = TRUE)
+#' fit_shared_sigma <- fit_data(
+#'   data = rbind(data, data_other), share_sigma = TRUE
+#' )
+#' @export
 fit_data <- function(
   data,
   share_a = FALSE,
@@ -71,7 +77,7 @@ fit_data <- function(
   fit_info$start_points <- calc_start_points(data = data, fit_info = fit_info)
 
   # run the optimiser
-  fit_info$optim_result <- optim(
+  fit_info$optim_result <- stats::optim(
     fit_info$start_points,
     objective_function,
     data = data,
@@ -101,18 +107,27 @@ fit_data <- function(
   # compute the overall fit log-likelihood
   fit_info$loglike <- calc_loglike(data = data, fit_info = fit_info)
 
-  return(list(f=fit_info,d=data))
   return(fit_info)
 
 }
 
-
+#' Evalulate the cumulative distribution function under the model.
+#'
+#' @param q Vector of quantiles
+#' @param later_mu Vector of the mean of the later component.
+#' @param later_sd Vector of the standard deviation of the later component.
+#' @param early_sd Vector of the standard deviation of the early component,
+#'  or `NULL` if there is no early component (the default).
+#' @returns Vector of cumulative distribution values
+#' @examples
+#' model_cdf(q = 1, later_mu = 1, later_sd = 1)
+#' model_cdf(q = 1, later_mu = 1, later_sd = 1, early_sd = 3)
+#' @export
 model_cdf <- function(q, later_mu, later_sd, early_sd = NULL) {
 
   if (is.null(early_sd)) {
-    p <- pnorm(q = q, mean = later_mu, sd = later_sd)
-  }
-  else {
+    p <- stats::pnorm(q = q, mean = later_mu, sd = later_sd)
+  } else {
     p <- pnorm_with_early(
       q = q,
       later_mu = later_mu,
@@ -125,12 +140,24 @@ model_cdf <- function(q, later_mu, later_sd, early_sd = NULL) {
 
 }
 
+#' Evalulate the probability density function under the model.
+#'
+#' @param x Vector of quantiles
+#' @param later_mu Vector of the mean of the later component.
+#' @param later_sd Vector of the standard deviation of the later component.
+#' @param early_sd Vector of the standard deviation of the early component,
+#'  or `NULL` if there is no early component (the default).
+#' @param log If `TRUE`, probabilities are given as `log(p)`.
+#' @returns Vector of probabilities
+#' @examples
+#' model_pdf(x = 1, later_mu = 1, later_sd = 1)
+#' model_pdf(x = 1, later_mu = 1, later_sd = 1, early_sd = 3)
+#' @export
 model_pdf <- function(x, later_mu, later_sd, early_sd = NULL, log = FALSE) {
 
-  if(is.null(early_sd)) {
-    p <- dnorm(x = x, mean = later_mu, sd = later_sd, log = log)
-  }
-  else {
+  if (is.null(early_sd)) {
+    p <- stats::dnorm(x = x, mean = later_mu, sd = later_sd, log = log)
+  } else {
     p <- dnorm_with_early(
       x = x,
       later_mu = later_mu,
@@ -160,10 +187,10 @@ calc_loglike <- function(data, fit_info) {
     dplyr::pull(loglike)
 
   return(sum(loglike))
-  
+
 }
 
-
+# parses a vector of parameters into a named list
 unpack_params <- function(params, n_a, n_sigma, n_sigma_e) {
 
   # first `n_a` items are the a parameters
@@ -187,13 +214,12 @@ convert_a_to_mu_and_k <- function(a, sigma, intercept_form) {
   if (intercept_form) {
     k <- a
     mu <- k * sigma
-  }
-  else {
+  } else {
     mu <- a
-    k = mu / sigma
+    k <- mu / sigma
   }
 
-  return(list(mu=mu, k=k))
+  return(list(mu = mu, k = k))
 
 }
 
@@ -247,7 +273,7 @@ objective_function <- function(params, data, fit_info) {
 }
 
 
-# use the sample mean and standard deviation of the promptness values
+# uses the sample mean and standard deviation of the promptness values
 # to create optimisation starting points
 calc_start_points <- function(data, fit_info) {
 
@@ -261,7 +287,7 @@ calc_start_points <- function(data, fit_info) {
   sigma_values <- (
     data |>
       dplyr::group_by(.data$i_sigma) |>
-      dplyr::summarize(val = sd(.data$promptness)) |>
+      dplyr::summarize(val = stats::sd(.data$promptness)) |>
       dplyr::pull(.data$val)
   )
 
@@ -270,8 +296,7 @@ calc_start_points <- function(data, fit_info) {
     if (fit_info$n_a == 1) {
       a_values <- mean(a_values)
     }
-  }
-  else {
+  } else {
     a_values <- mu_values
   }
 
@@ -281,7 +306,7 @@ calc_start_points <- function(data, fit_info) {
     sigma_e_values <- (
       data |>
         dplyr::group_by(.data$i_sigma_e) |>
-        dplyr::summarize(val = sd(.data$promptness) * 3) |>
+        dplyr::summarize(val = stats::sd(.data$promptness) * 3) |>
         dplyr::pull(.data$val)
     )
 
@@ -310,8 +335,8 @@ pnorm_with_early <- function(q, later_mu, later_sd, early_sd) {
 
   # cdf of the maximum of two independent gaussians is the product of
   # their individual values
-  early_p <- pnorm(q, early_mu, early_sd)
-  later_p <- pnorm(q, later_mu, later_sd)
+  early_p <- stats::pnorm(q, early_mu, early_sd)
+  later_p <- stats::pnorm(q, later_mu, later_sd)
   p <- early_p * later_p
 
   return(p)
@@ -347,10 +372,11 @@ dnorm_with_early <- function(x, later_mu, later_sd, early_sd, log = FALSE) {
 }
 
 erf <- function(x) {
-  return(2 * pnorm(x * sqrt(2)) - 1)
+  return(2 * stats::pnorm(x * sqrt(2)) - 1)
 }
 
-
+# works out how many parameters there are, given the sharing amongst
+# the parameters
 set_param_counts <- function(fit_info) {
 
   fit_info$n_a <- ifelse(fit_info$share_a, 1, fit_info$n_datasets)
@@ -371,40 +397,17 @@ set_param_counts <- function(fit_info) {
 
 }
 
+
+# creates `i_mu`, `i_sigma`, and `i_sigma_e` columns in the data
+# that describe the indices of the relevant parameters
 set_data_param_indices <- function(data, fit_info) {
 
-  i_dataset = as.integer(data$name_factor)
+  i_dataset <- as.integer(data$name_factor)
 
   # remember that `ifelse` is strange for vectors!
-  data$i_mu <- if(fit_info$n_mu == 1) 1 else i_dataset
-  data$i_sigma <- if(fit_info$share_sigma) 1 else i_dataset
-  data$i_sigma_e <- if(fit_info$share_sigma_e) 1 else i_dataset
-
-  return(data)
-
-}
-
-
-load_carpenter_data <- function() {
-  raw <- read.csv("../../demo_data/Carpenter_Williams_Nature_1995.csv")
-}
-
-
-get_example_data <- function(showing) {
-
-  data <- load_carpenter_data()
-
-  if (showing == "swivel") {
-    data <- data |>
-      dplyr::filter(.data$Condition %in% c("p05", "p95")) |>
-      dplyr::mutate(
-        name = .data$Condition,
-        promptness = 1 / (.data$Times / 1000)
-      )
-  }
-  else {
-    stopifnot(FALSE)
-  }
+  data$i_mu <- if (fit_info$n_mu == 1) 1 else i_dataset
+  data$i_sigma <- if (fit_info$share_sigma) 1 else i_dataset
+  data$i_sigma_e <- if (fit_info$share_sigma_e) 1 else i_dataset
 
   return(data)
 
